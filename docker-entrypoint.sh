20e4caf04971af4259d11db969439df767a3dd58
Shawn Lawyer <shawnlawyer@gmail.com>
2020-05-10 15:51:20 -0400

    cleaning up container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,20 +3,25 @@
 export TERM=xterm
 
 if [ ! -d 'logs' ]; then
+
     mkdir -p logs
     mkdir -p logs/nginx
     mkdir -p logs/app
+
 fi
 
 python3 migrate.py db upgrade
 
 if [ $ENVIRONMENT == 'DEV'] ; then
+
     sudo cp docker/nginx-dev.conf /etc/nginx/nginx.conf
     screen -dmS APP bash -c 'python3 app.py'
+
 else
 
     sudo cp docker/nginx-uwsgi.conf /etc/nginx/nginx.conf
     screen -dmS APP bash -c 'uwsgi uwsgi.ini'
+
 fi
 
 sudo nginx
